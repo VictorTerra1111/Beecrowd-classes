0243ba63c6029f7a2fefe1f4d3f938bb904eb039
Victor Terra <123105471+VictorTerra1111@users.noreply.github.com>
2024-08-26 10:50:53 -0300

    circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Circle{
+  public:
+        Circle(double r);
+        void area(double r);
+};
+voic Circle::area(double r){
+    cin >> r;
+    cout << "A=" << fixed << set_precision(4) << 3.14159*r*r << endl;
+}
+
+int main(){
+  

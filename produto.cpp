a12a2cd00a6c5a81a7a1e62c969dafe6743da5c1
Victor Terra <123105471+VictorTerra1111@users.noreply.github.com>
2024-08-26 11:03:26 -0300

    produto.cpp

--- produto.cpp
+++ produto.cpp
@@ -1,1 +1,26 @@
-    
+#include <iostream> 
+ 
+using namespace std; 
+ 
+class Calculator{ 
+        public: 
+                int prod(int a, int b); 
+        private:
+
+}; 
+ 
+int Calculator::prod(int a, int b){ 
+        cin >> a >> b; 
+        int prod = a*b; 
+        return prod; 
+} 
+ 
+int main(){ 
+ 
+        Calculator produto; 
+        int a, b; 
+ 
+        int answer = produto.prod(a, b); 
+        cout << "PROD = " << answer << endl; 
+        return 0; 
+}    
